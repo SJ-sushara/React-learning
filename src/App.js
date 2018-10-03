b7afeeb773805b8f760d784851813efd49b11951
FronteraDevs <test@fronteraswarm.onmicrosoft.com>
2018-10-03 16:42:16 +0530

    styled components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import styled from "styled-components";
+
+const TodoComponent = styled.div`
+  background-color: red;
+  width: 300px;
+  min-height: 200px;
+  margin: 30px auto;
+  box-sizing: border-box;
+`;
+
+const BasicNotification = styled.p`
+  background: lightblue;
+  padding: 5px;
+  margin: 5px;
+  color: black;
+`;
+const SuccessNotification = styled(BasicNotification)`
+  background: lightgreen;
+`;
+
+const ErrorNotification = styled(BasicNotification)`
+  background: lightcoral;
+  font-weight: bold;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <TodoComponent>
+        <h2>ToDo</h2>
+        {/* <div>
+          <Input onChange={this.handleChange} />
+          <p>{this.state.error}</p>
+          <ToDoList value={this.state.display} />
+        </div> */}
+        <BasicNotification>Basic Message</BasicNotification>
+        <SuccessNotification>Success Message</SuccessNotification>
+        <ErrorNotification>Error Message</ErrorNotification>
+      </TodoComponent>
     );
   }
 }
